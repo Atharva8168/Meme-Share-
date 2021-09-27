4ca8afca1d511c45ad2749436264c2f2eab5fc83
Atharva Bauskar <bauskaratharva@gmail.com>
2021-09-27 18:14:19 +0530

    Share Button Activited

--- app/src/main/java/com/example/android/memeshare/MainActivity.kt
+++ app/src/main/java/com/example/android/memeshare/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.android.memeshare
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -9,6 +10,7 @@ import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private var currentImageUrl : String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,7 +20,7 @@ class MainActivity : AppCompatActivity() {
 
 
     private fun loadMeme(){
-        val currentImageUrl = "https://meme-api.herokuapp.com/gimme"
+        currentImageUrl = "https://meme-api.herokuapp.com/gimme"
 
         val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, currentImageUrl, null,
             { response ->
@@ -38,6 +40,11 @@ class MainActivity : AppCompatActivity() {
 
 
     fun shareMeme(view: android.view.View) {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "text/plain"
+        intent.putExtra(Intent.EXTRA_TEXT, "Checkout this cool meme i got on reddit... $currentImageUrl")
+        val chooser = Intent.createChooser(intent , "Share this meme using....")
+        startActivity(chooser)
 
     }
     fun nextMeme(view: android.view.View) {
